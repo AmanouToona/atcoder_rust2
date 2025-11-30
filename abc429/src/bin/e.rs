f8b8bbf4573a0fe374cc2698bcc55f0f51b7331e
toona <amanou.toona@gmail.com>
2025-11-30 09:46:04 +0900

    [WIP] abc429-e : ac 35, wa19
    s までの最小距離と2次最小距離を持つ方針
    方針自体は悪くないと思う

--- abc429/src/bin/e.rs
+++ abc429/src/bin/e.rs
@@ -1,5 +1,44 @@
 use proconio::input;
+use proconio::marker::Chars;
+use std::collections::VecDeque;
 #[allow(non_snake_case)]
 fn main() {
-    input!{}
+    input! {
+        (N, M): (usize, usize),
+        uv: [(usize, usize); M],
+        S: Chars,
+    }
+
+    let mut g: Vec<Vec<usize>> = vec![Vec::new(); N];
+
+    for &(u, v) in uv.iter() {
+        g[u - 1].push(v - 1);
+        g[v - 1].push(u - 1);
+    }
+
+    let mut q = VecDeque::new();
+    let mut dist: Vec<Vec<usize>> = vec![Vec::new(); N];
+    for (i, s) in S.iter().enumerate() {
+        if *s == 'S' {
+            q.push_back((i, 0));
+            dist[i].push(0);
+        }
+    }
+
+    while let Some((u, d)) = q.pop_front() {
+        for v in g[u].iter() {
+            if dist[*v].len() >= 2 {
+                continue;
+            }
+            dist[*v].push(d + 1);
+            q.push_back((*v, d + 1));
+        }
+    }
+
+    for (i, &s) in S.iter().enumerate() {
+        if s == 'D' {
+            let d: usize = dist[i].iter().sum();
+            println!("{d}");
+        }
+    }
 }
