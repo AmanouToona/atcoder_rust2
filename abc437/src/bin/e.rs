25a5a41319f21335af8ffd631ae4ad2ab34b1333
john <...@...>
2026-01-10 20:46:32 +0900

    [WIP] abc437-e: 方針は変更せずに実装を簡単に修正 26ac, 22wa

--- abc437/src/bin/e.rs
+++ abc437/src/bin/e.rs
@@ -1,76 +1,16 @@
-use amplify::confinement::Collection;
+use itertools::Itertools;
 use proconio::input;
-use std::collections::HashMap;
-use std::collections::VecDeque;
-use std::hash::Hash;
+use std::collections::BTreeMap;
 
-#[derive(Default)]
-struct TrieNode {
-    children: HashMap<usize, TrieNode>,
-    y: Vec<usize>,
-}
-
-#[derive(Default)]
-struct Trie {
-    root: TrieNode,
-}
-
-impl Trie {
-    fn new() -> Self {
-        Trie {
-            root: TrieNode::default(),
+fn dfs(u: usize, g: &Vec<BTreeMap<usize, Vec<usize>>>, ans: &mut Vec<usize>) {
+    let gu = g[u].clone();
+    for v_s in gu.values() {
+        for v in v_s.iter() {
+            ans.push(*v);
         }
-    }
-
-    fn insert(&mut self, x: usize, y: &[usize]) {
-        let mut node = &mut self.root;
-        for yy in y.iter() {
-            node = node.children.entry(*yy).or_default();
+        for v in v_s.iter() {
+            dfs(*v, g, ans);
         }
-        node.y.push(x);
-    }
-}
-
-struct Solve {
-    trie: Trie,
-    g: Vec<Vec<(usize, usize)>>,
-    path: Vec<usize>,
-    used: Vec<bool>,
-}
-
-impl Solve {
-    fn new(trie: Trie, g: Vec<Vec<(usize, usize)>>) -> Self {
-        let n = g.len();
-        Solve {
-            trie,
-            g,
-            path: Vec::new(),
-            used: vec![false; n + 1],
-        }
-    }
-
-    fn dfs(&mut self, u: usize) {
-        self.used[u] = true;
-        let g = self.g[u].clone();
-        for &(v, y) in g.iter() {
-            if self.used[v] {
-                continue;
-            }
-            self.path.push(y);
-            self.trie.insert(v, &self.path);
-            self.dfs(v);
-            self.path.pop();
-        }
-    }
-
-    fn make_ans(&self) {
-        // let mut q = VecDeque::new();
-        let mut ans: Vec<usize> = Vec::new();
-
-        let mut nxt: Vec<usize> = self.trie.root.children.keys().cloned().collect();
-        nxt.sort();
-
-        println!("nxt {:?}", nxt);
     }
 }
 
@@ -78,22 +18,17 @@ impl Solve {
 fn main() {
     input! {
         N: usize,
-        xy: [(usize, usize); N],
+        xy: [(usize, usize); N]
     }
 
-    let mut g = vec![Vec::new(); N + 1];
+    let mut g: Vec<BTreeMap<usize, Vec<usize>>> = vec![BTreeMap::new(); N + 1];
     for (i, &(x, y)) in xy.iter().enumerate() {
-        g[x].push((i, y));
+        let i = i + 1;
+        g[x].entry(y).or_default().push(i);
     }
 
-    let trie = Trie::new();
-    let mut Solve = Solve::new(trie, g);
-    for i in 0..=N {
-        if Solve.used[i] {
-            continue;
-        }
-        Solve.dfs(i);
-    }
-
-    Solve.make_ans();
+    let mut ans = Vec::new();
+    dfs(0, &g, &mut ans);
+    let ans: String = ans.iter().join(" ");
+    println!("{ans}");
 }
