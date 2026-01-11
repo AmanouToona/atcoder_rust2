0ee6d21623b84b52e2f101a93267868ed31dd190
john <...@...>
2026-01-11 23:25:40 +0900

    [AC] abc437 e: trie木の記述に時間がかかった

--- abc437/src/bin/e.rs
+++ abc437/src/bin/e.rs
@@ -2,15 +2,11 @@ use itertools::Itertools;
 use proconio::input;
 use std::collections::BTreeMap;
 
-fn dfs(u: usize, g: &Vec<BTreeMap<usize, Vec<usize>>>, ans: &mut Vec<usize>) {
-    let gu = g[u].clone();
-    for v_s in gu.values() {
-        for v in v_s.iter() {
-            ans.push(*v);
-        }
-        for v in v_s.iter() {
-            dfs(*v, g, ans);
-        }
+fn dfs(u: usize, is: &[Vec<usize>], to: &[BTreeMap<usize, usize>], ans: &mut Vec<usize>) {
+    ans.extend_from_slice(&is[u]);
+
+    for (_, v) in to[u].iter() {
+        dfs(*v, is, to, ans);
     }
 }
 
@@ -21,14 +17,30 @@ fn main() {
         xy: [(usize, usize); N]
     }
 
-    let mut g: Vec<BTreeMap<usize, Vec<usize>>> = vec![BTreeMap::new(); N + 1];
+    let mut is: Vec<Vec<usize>> = vec![vec![0]]; // node に属する a の番号
+    let mut to: Vec<BTreeMap<usize, usize>> = vec![BTreeMap::new()]; // node の持つ edge
+    let mut vid: Vec<usize> = vec![0; N + 1]; // a -> node
+
     for (i, &(x, y)) in xy.iter().enumerate() {
+        let parent = vid[x]; // 親ノードの番号
         let i = i + 1;
-        g[x].entry(y).or_default().push(i);
+
+        if let Some(nxt) = to[parent].get(&y) {
+            vid[i] = *nxt;
+            is[*nxt].push(i);
+        } else {
+            let nxt_nid = is.len();
+            is.push(vec![i]);
+            to.push(BTreeMap::new());
+
+            to[parent].insert(y, nxt_nid);
+            vid[i] = nxt_nid;
+        };
     }
 
     let mut ans = Vec::new();
-    dfs(0, &g, &mut ans);
-    let ans: String = ans.iter().join(" ");
+    dfs(0, &is, &to, &mut ans);
+
+    let ans: String = ans.iter().skip(1).join(" ");
     println!("{ans}");
 }
