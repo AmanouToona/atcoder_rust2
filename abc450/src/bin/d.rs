75f7557b026ce7e439af242c5eb8e667b647efe6
john <...@...>
2026-03-22 18:36:54 +0900

    [AC] abc450-de

--- abc450/src/bin/d.rs
+++ abc450/src/bin/d.rs
@@ -1,38 +1,24 @@
 #![allow(non_snake_case)]
-use std::usize;
-
 use proconio::input;
-
+use std::collections::VecDeque;
 fn main() {
     input! {
         (N, K): (usize, usize),
-        mut A: [usize; N],
+         A: [usize; N],
     }
 
+    let a_max = *A.iter().max().unwrap();
+
+    let mut A: Vec<usize> = A.iter().map(|&a| a + (a_max - a) / K * K).collect();
     A.sort();
-    let amax = A.last().unwrap();
 
-    let mut max = amax + K + (K + 1) / 2;
-    let mut min = max - K;
-
-    let mut ans = usize::MAX;
-    while max - min > 1 {
-        let mid = (max + min) / 2;
-
-        let mut tmp = Vec::new();
-        for &a in A.iter() {
-            let div = (mid - a) / K;
-            tmp.push((a + div * K));
-        }
-
-        let tmp = tmp.iter().max().unwrap() - tmp.iter().min().unwrap();
-
-        if tmp < ans {
-            max = mid;
-            ans = tmp;
-        } else {
-            min = mid;
-        }
+    let mut normal = VecDeque::from_iter(A.iter().cloned());
+    let mut ans = normal[N - 1] - normal[0];
+    for _ in 0..N {
+        let a = normal.pop_front().unwrap();
+        normal.push_back(a + K);
+        ans = ans.min(normal[N - 1] - normal[0]);
     }
+
     println!("{ans}");
 }
--- abc450/src/bin/e.rs
+++ abc450/src/bin/e.rs
@@ -1,6 +1,97 @@
 #![allow(non_snake_case)]
 use proconio::input;
 use proconio::marker::Chars;
+
+struct Solver {
+    len: Vec<usize>,
+    count: Vec<Vec<usize>>,
+    cumsum_x: Vec<Vec<usize>>,
+    cumsum_y: Vec<Vec<usize>>,
+}
+
+impl Solver {
+    fn new(x: &[char], y: &[char]) -> Self {
+        let mut cumsum_x = vec![vec![0; 26]; x.len() + 1];
+        let mut cumsum_y = vec![vec![0; 26]; y.len() + 1];
+
+        for i in 0..x.len() {
+            cumsum_x[i + 1] = cumsum_x[i].clone();
+            cumsum_x[i + 1][(x[i] as u8 - b'a') as usize] += 1;
+        }
+
+        for i in 0..y.len() {
+            cumsum_y[i + 1] = cumsum_y[i].clone();
+            cumsum_y[i + 1][(y[i] as u8 - b'a') as usize] += 1;
+        }
+
+        let mut len = vec![0, x.len(), y.len()];
+        let mut count = vec![
+            vec![0; 26],
+            cumsum_x.last().unwrap().clone(),
+            cumsum_y.last().unwrap().clone(),
+        ];
+
+        while *len.last().unwrap() <= 10usize.pow(18u32) {
+            let i = len.len();
+            len.push(len[i - 1] + len[i - 2]);
+
+            let mut nxt_cnt = vec![0; 26];
+            for c in 0..26 {
+                nxt_cnt[c] += count[i - 1][c] + count[i - 2][c];
+            }
+            count.push(nxt_cnt);
+        }
+
+        eprintln!("len: {:?}", len);
+
+        Solver {
+            len,
+            count,
+            cumsum_x,
+            cumsum_y,
+        }
+    }
+
+    fn solve(&self, k: usize, c: usize) -> usize {
+        if k == 0 {
+            return 0;
+        }
+
+        let mut k = k;
+
+        let mut res = 0;
+        let mut idx = self.len.len() - 1;
+
+        while idx > 0 {
+            // eprintln!("idx {idx},  k {k} res{res}");
+            if idx == 1 {
+                res += self.cumsum_x[k][c];
+                break;
+            }
+            if idx == 2 {
+                res += self.cumsum_y[k][c];
+                break;
+            }
+
+            if k > self.len[idx] {
+                idx -= 1;
+                continue;
+            } else if k == self.len[idx] {
+                res += self.count[idx][c];
+                break;
+            } else if k >= self.len[idx - 1] {
+                res += self.count[idx - 1][c];
+                k -= self.len[idx - 1];
+                idx -= 2;
+            } else {
+                idx -= 1;
+            }
+        }
+
+        res
+    }
+}
+
 fn main() {
     input! {
         X: Chars,
@@ -8,14 +99,15 @@ fn main() {
         Q: usize,
     }
 
-    let mut lrc = Vec::new();
+    let solve = Solver::new(&X, &Y);
 
-    for q in 0..Q {
+    for _ in 0..Q {
         input! {
             (L, R, C):(usize, usize, char),
         }
-        lrc.push((L, R, C, q));
-    }
 
-    lrc.sort();
+        let ans = solve.solve(R, (C as u8 - b'a') as usize)
+            - solve.solve(L - 1, (C as u8 - b'a') as usize);
+        println!("{ans}");
+    }
 }
