cbec468bdb322ef352d092289ec96e073590b72c
john <...@...>
2026-03-28 23:21:53 +0900

    [AC] abc451-e

--- abc451/src/bin/e.rs
+++ abc451/src/bin/e.rs
@@ -2,8 +2,6 @@
 use ac_library::Dsu;
 use amplify::confinement::Collection;
 use proconio::input;
-use std::cmp::Reverse;
-use std::collections::BinaryHeap;
 use std::collections::VecDeque;
 
 fn main() {
@@ -12,57 +10,53 @@ fn main() {
     }
 
     let mut A = vec![vec![0; N]; N];
-    let mut edge: BinaryHeap<(Reverse<usize>, usize, usize)> = BinaryHeap::new();
+    let mut edge: Vec<(usize, usize, usize)> = Vec::new();
     for i in 0..N - 1 {
         input! {a: [usize; N - 1 - i]}
         for (j, aa) in a.iter().enumerate() {
             A[i][i + j + 1] = *aa;
             A[i + j + 1][i] = *aa;
-            edge.push((Reverse(*aa), i, j));
+            edge.push((*aa, i, i + j + 1));
         }
     }
+    edge.sort();
 
     let mut dsu = Dsu::new(N);
     let mut g: Vec<Vec<usize>> = vec![Vec::new(); N];
-    for (Reverse(c), i, j) in edge.iter() {
+    for (c, i, j) in edge.iter() {
         if dsu.same(*i, *j) {
             continue;
         } else {
             g[*i].push(*j);
             g[*j].push(*i);
+            dsu.merge(*i, *j);
         }
     }
 
-    let mut parent: Vec<Vec<Option<usize>>> = vec![vec![None; N]; 13]; // parent[k][u]: u の 2**k おや
-    let mut dist: Vec<usize> = vec![0; N];
-    let mut seen = vec![false; N];
-    let mut q = VecDeque::new();
-    q.push(0);
-    seen[0] = true;
-    while let Some(u) = q.pop_front() {
-        for &v in g[u].iter() {
-            if seen[v] {
-                continue;
-            }
-            q.push(v);
-            seen[v] = true;
-            parent[0][v] = Some(u);
-            dist[v] = dist[u] + A[u][v];
-        }
-    }
+    for i in 0..N {
+        let mut dist = vec![0; N];
+        let mut seen = vec![false; N];
+        let mut q = VecDeque::new();
+        seen[i] = true;
+        q.push(i);
 
-    for i in 0..12 {
-        for v in 0..N {
-            if parent[i][v].is_some() {
-                parent[i + 1][v] = parent[i][parent[i][v].unwrap()]
+        while let Some(u) = q.pop_front() {
+            for &v in g[u].iter() {
+                if seen[v] {
+                    continue;
+                }
+                seen[v] = true;
+                dist[v] = dist[u] + A[u][v];
+                q.push(v);
             }
         }
-    }
 
-    for i in parent.iter() {
-        eprintln!("{:?}", i);
+        for (j, &d) in dist.iter().enumerate() {
+            if A[i][j] != d {
+                println!("No");
+                return;
+            }
+        }
     }
-
-    // let mut B: Vec<Vec<Option<usize>>> = vec![vec![None; N]; N];
-    // let mut q = VecDeque::new();
+    println!("Yes");
 }
