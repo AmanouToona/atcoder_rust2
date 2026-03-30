56d37e8bce4272df10910b8824cb764704d9a696
john <...@...>
2026-03-30 22:43:21 +0900

    [REFACTOR] abc451-e : lca を用いる別解 速度は落ちる

--- abc451/src/bin/e.rs
+++ abc451/src/bin/e.rs
@@ -2,7 +2,83 @@
 use ac_library::Dsu;
 use amplify::confinement::Collection;
 use proconio::input;
-use std::collections::VecDeque;
+use std::{collections::VecDeque, usize};
+
+// Lowest Common Ancestor
+struct LCA {
+    parents: Vec<Vec<usize>>, // parents[k][u] := u の 2 ** k 先の親
+    distance: Vec<usize>,     // root からの距離
+}
+
+impl LCA {
+    fn new(g: &[Vec<usize>]) -> Self {
+        let n = g.len();
+        let mut k = 0;
+        while (1 << k) <= n {
+            k += 1
+        }
+
+        let mut q = VecDeque::new();
+        let mut parents = vec![vec![0; n]; k];
+        let mut distance = vec![usize::MAX; n];
+        q.push_back(0);
+        distance[0] = 0;
+
+        while let Some(u) = q.pop_front() {
+            for &v in g[u].iter() {
+                if distance[v] == usize::MAX {
+                    parents[0][v] = u;
+                    q.push_back(v);
+
+                    distance[v] = distance[u] + 1;
+                }
+            }
+        }
+
+        if !distance.iter().skip(1).all(|x| *x > 0) {
+            panic!("non connected tree !!")
+        }
+
+        for i in 0..k - 1 {
+            for u in 0..n {
+                parents[i + 1][u] = parents[i][parents[i][u]];
+            }
+        }
+
+        LCA { parents, distance }
+    }
+
+    fn query(&self, u: usize, v: usize) -> usize {
+        let mut u = u;
+        let mut v = v;
+        let k = self.parents.len();
+
+        if self.distance[u] > self.distance[v] {
+            std::mem::swap(&mut u, &mut v);
+        }
+
+        // 深さを揃える
+        for i in 0..k {
+            if (self.distance[v] - self.distance[u]) >> i & 1 == 1 {
+                v = self.parents[i][v];
+            }
+        }
+
+        if u == v {
+            return u;
+        }
+
+        // 共通祖先
+        for i in (0..k).rev() {
+            if self.parents[i][u] != self.parents[i][v] {
+                u = self.parents[i][u];
+                v = self.parents[i][v];
+            }
+        }
+
+        self.parents[0][v]
+    }
+}
 
 fn main() {
     input! {
@@ -33,30 +109,35 @@ fn main() {
         }
     }
 
-    for i in 0..N {
-        let mut dist = vec![0; N];
-        let mut seen = vec![false; N];
-        let mut q = VecDeque::new();
-        seen[i] = true;
-        q.push(i);
+    // 根を 0 とした距離を算出
+    let mut dist = vec![0; N];
+    let mut seen = vec![false; N];
+    let mut q = VecDeque::new();
+    seen[0] = true;
+    q.push(0);
 
-        while let Some(u) = q.pop_front() {
-            for &v in g[u].iter() {
-                if seen[v] {
-                    continue;
-                }
-                seen[v] = true;
-                dist[v] = dist[u] + A[u][v];
-                q.push(v);
+    while let Some(u) = q.pop_front() {
+        for &v in g[u].iter() {
+            if seen[v] {
+                continue;
             }
+            seen[v] = true;
+            dist[v] = dist[u] + A[u][v];
+            q.push(v);
         }
+    }
 
-        for (j, &d) in dist.iter().enumerate() {
+    let lca = LCA::new(&g);
+    for i in 0..N {
+        for j in i + 1..N {
+            let k = lca.query(i, j);
+            let d = (dist[i] - dist[k]) + (dist[j] - dist[k]);
             if A[i][j] != d {
                 println!("No");
                 return;
             }
         }
     }
+
     println!("Yes");
 }
