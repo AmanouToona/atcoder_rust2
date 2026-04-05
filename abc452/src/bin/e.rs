ed4a0ac0ec85745d4b81d9282fecd2934bde64b8
john <...@...>
2026-04-05 11:20:04 +0900

    [AC] abc452-e : 解説AC

--- abc452/src/bin/e.rs
+++ abc452/src/bin/e.rs
@@ -1,5 +1,5 @@
 #![allow(non_snake_case)]
-use ac_library::{Min, ModInt998244353 as Mint};
+use ac_library::ModInt998244353 as Mint;
 use proconio::input;
 fn main() {
     input! {
@@ -8,13 +8,35 @@ fn main() {
         B: [Mint; M],
     }
 
-    // (i - j).abs() のループ？
     let mut ans = Mint::new(0);
-    for d in 0..10usize.pow(6) {
-        let tmp = Mint::new(0);
-        for i in 0..N {}
+
+    let mut ai_sum = Mint::new(0);
+    for (i, &a) in A.iter().enumerate() {
+        ai_sum += Mint::new(i + 1) * a;
     }
 
-    let suma: Mint = A.iter().sum();
-    println!("{suma}");
+    for &b in B.iter() {
+        ans += b * ai_sum;
+    }
+
+    let mut cumsum = vec![Mint::new(0); N + 2];
+    for (i, a) in A.iter().enumerate() {
+        cumsum[i + 2] = *a;
+        let (i1, i2) = cumsum.split_at_mut(i + 2);
+        i2[0] += i1.last().unwrap();
+    }
+
+    for (j, b) in B.iter().enumerate() {
+        let j = j + 1; // 式を問題文に合わせるため
+        let mut aij = Mint::new(0);
+        let mut i = 1;
+        while i <= N {
+            let nxt = (N + 1).min((i / j + 1) * j);
+            aij += (cumsum[nxt] - cumsum[i]) * Mint::new(i / j) * Mint::new(j);
+            i = nxt;
+        }
+        ans -= b * aij;
+    }
+
+    println!("{ans}");
 }
