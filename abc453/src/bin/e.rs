cafb985b04f0512222137e1a8948d258e833858d
john <...@...>
2026-04-12 00:14:24 +0900

    [WIP] abc453-e: AC:7, WA:28

--- abc453/src/bin/e.rs
+++ abc453/src/bin/e.rs
@@ -7,6 +7,7 @@ fn main() {
         N: usize,
         lr: [(usize, usize); N],
     }
+    // eprintln!("");
 
     /*
     全ての可能性を考える 愚直だと 2 ** N になる。
@@ -15,7 +16,7 @@ fn main() {
     l, r の制約の確認を逐次行うことが難しい。　最後にできる人数？, 最もきつい制約を持っておけばよい？
     l でソートして?
      */
-    let mut ls = vec![Vec::new(); N + 1];
+    let mut ls: Vec<Vec<usize>> = vec![Vec::new(); N + 1];
     let mut rs = vec![Vec::new(); N + 1];
     for (i, &(l, r)) in lr.iter().enumerate() {
         ls[l].push(i);
@@ -30,22 +31,45 @@ fn main() {
         finv[i] = finv[i - 1] / mint::new(i);
     }
 
-    let mut set1 = HashSet::new();
-    let mut set2 = HashSet::new();
-    let mut set3 = HashSet::new();
+    let mut a = HashSet::new();
+    let mut b = HashSet::new();
+    let mut both = HashSet::new();
+    let mut ans = mint::new(0);
 
-    for (i, j) in (1..N).zip(N - 1..) {
+    for (i, j) in (1..N).zip((1..N).rev()) {
         for l in ls[i].iter() {
-            set1.insert(*l);
+            a.insert(*l);
+            if b.contains(l) {
+                both.insert(*l);
+            }
+        }
+        for r in rs[i - 1].iter() {
+            a.remove(r);
+            both.remove(r);
         }
 
         for r in rs[j].iter() {
-            set2.insert(*r);
-            if set1.contains(r) {
-                set3.insert(*r);
+            b.insert(*r);
+            if a.contains(r) {
+                both.insert(*r);
             }
         }
-    }
+        for l in ls[j + 1].iter() {
+            b.remove(l);
+            both.remove(l);
+        }
+        // println!("{i} {j}");
+        // println!("{:?}, {:?}, {:?}", a, b, both);
 
-    let mut ans = mint::new(0);
+        let static_a = a.len() - both.len();
+        if static_a > i {
+            continue;
+        }
+        let to_have = i - static_a;
+        if to_have > both.len() {
+            continue;
+        }
+        ans += frac[both.len()] * finv[to_have] * finv[both.len() - to_have];
+    }
+    println!("{ans}");
 }
