3587370a8890b9ca352705f1449788d39ab68049
john <...@...>
2026-05-27 18:48:47 +0900

    [AC]

--- abc223/src/bin/e.rs
+++ abc223/src/bin/e.rs
@@ -1,4 +1,5 @@
 #![allow(non_snake_case)]
+use itertools::Itertools;
 use proconio::input;
 
 /*
@@ -9,83 +10,21 @@ use proconio::input;
 - ある長方形を x, y に張り付かせる？
 */
 
-struct Solve {
-    x: usize,
-    y: usize,
-    bx: Vec<usize>,
-}
-
-impl Solve {
-    fn new(x: usize, y: usize, a: usize, b: usize, c: usize) -> Self {
-        Solve {
-            x,
-            y,
-            bx: vec![a, b, c],
-        }
+fn can(x: usize, y: usize, a: usize, b: usize, c: usize) -> bool {
+    if a.div_ceil(x) >= y {
+        return false;
     }
 
-    fn can_x(&self, i: usize) -> bool {
-        //! 箱 i を x 軸に張り付かせる
+    let res_y = y - a.div_ceil(x);
 
-        if self.bx[i].div_ceil(self.x) >= self.y {
-            return false;
-        }
-
-        let res_y = self.y - self.bx[i].div_ceil(self.x);
-        // 使える領域は x, res_y の四角形
-
-        // res_y 方向に張り付く
-        let mut want_x = 0;
-        for j in 0..3 {
-            if j != i {
-                want_x += self.bx[j].div_ceil(res_y);
-            }
-        }
-        if want_x <= self.x {
-            return true;
-        }
-
-        // x 方向に張り付く
-        let mut want_y = 0;
-        for j in 0..3 {
-            if j != i {
-                want_y += self.bx[j].div_ceil(self.x);
-            }
-        }
-
-        want_y <= res_y
+    // x, res_y の四角形が使える
+    let want_x = b.div_ceil(res_y) + c.div_ceil(res_y);
+    if want_x <= x {
+        return true;
     }
 
-    fn can_y(&self, i: usize) -> bool {
-        //! 箱 i を y 軸に張り付かせる
-        if self.bx[i].div_ceil(self.y) >= self.x {
-            return false;
-        }
-
-        let res_x = self.x - self.bx[i].div_ceil(self.y);
-        // 使える領域は、 res_x, y の四角形
-
-        // res_x 方向に張り付く
-        let mut want_y = 0;
-        for j in 0..3 {
-            if j != i {
-                want_y += self.bx[j].div_ceil(res_x);
-            }
-        }
-        if want_y <= self.y {
-            return true;
-        }
-
-        // y 方向に張り付く
-        let mut want_x = 0;
-        for j in 0..3 {
-            if j != i {
-                want_x += self.bx[j].div_ceil(self.y);
-            }
-        }
-
-        want_x <= res_x
-    }
+    let want_y = b.div_ceil(x) + c.div_ceil(x);
+    want_y <= res_y
 }
 
 fn main() {
@@ -93,17 +32,16 @@ fn main() {
         (x, y, a, b, c): (usize, usize, usize, usize, usize),
     }
 
-    let solve = Solve::new(x, y, a, b, c);
-
-    for i in 0..3 {
-        if solve.can_x(i) {
+    for i in [a, b, c].iter().permutations(3) {
+        if can(x, y, *i[0], *i[1], *i[2]) {
             println!("Yes");
             return;
         }
-        if solve.can_y(i) {
+        if can(y, x, *i[0], *i[1], *i[2]) {
             println!("Yes");
             return;
         }
     }
+
     println!("No");
 }
