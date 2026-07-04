6baef5ec591b3e6d4299ad140a0ab6f3e770148b
john <...@...>
2026-07-04 23:42:06 +0900

    [AC] abc465-e

--- abc465/src/bin/e.rs
+++ abc465/src/bin/e.rs
@@ -32,46 +32,39 @@ fn main() {
         N: Chars,
     }
 
-    let mut digit_can = Vec::new();
-    for i in 0..1 << 10 {
-        if count_one(i) > 3 {
-            continue;
-        }
-        digit_can.push(i);
-    }
-
     // 桁 DP
-    // dp[使った数 bit][あまり][Nより小さい 1: なら小さい]
-    let mut dp = vec![vec![vec![mint::new(0); 2]; 3]; 1 << 10];
-    dp[0][0][0] = mint::new(1);
+    // dp[使った数 bit][あまり][Nより小さい 1: なら小さい][数字が立った]
+    let mut dp = vec![vec![vec![vec![mint::new(0); 2]; 2]; 3]; 1 << 10];
+    dp[0][0][0][0] = mint::new(1);
 
     for n in N.iter() {
-        let mut nxt_dp = vec![vec![vec![mint::new(0); 2]; 3]; 1 << 10];
+        let mut nxt_dp = vec![vec![vec![vec![mint::new(0); 2]; 2]; 3]; 1 << 10];
 
         let n: usize = n.to_digit(10).unwrap() as usize;
         for i in 0..10usize {
-            for &state in &digit_can {
-                if count_one(state) == 3 && ((state & i) == 0) {
-                    continue;
-                }
+            for state in 0..1 << 10 {
                 for res in 0..3 {
                     for smaller in 0..2 {
                         if smaller == 0 && i > n {
                             continue;
                         }
-
-                        let nxt_smaller = if smaller == 1 {
-                            1
-                        } else {
-                            if i < n {
-                                1
-                            } else {
-                                0
+                        for not_zero in 0..2 {
+                            if dp[state][res][smaller][not_zero] == mint::new(0) {
+                                continue;
                             }
-                        };
+                            let nxt_zero = if not_zero == 1 || i != 0 { 1 } else { 0 };
 
-                        nxt_dp[state | (1 << i)][(res * 10 + i) % 3][nxt_smaller] +=
-                            dp[state][res][smaller];
+                            let nxt_smaller = if smaller == 1 || i < n { 1 } else { 0 };
+
+                            let nxt_state = if not_zero == 0 && i == 0 {
+                                0
+                            } else {
+                                state | (1 << i)
+                            };
+
+                            nxt_dp[nxt_state][(res * 10 + i) % 3][nxt_smaller][nxt_zero] +=
+                                dp[state][res][smaller][not_zero];
+                        }
                     }
                 }
             }
@@ -80,39 +73,30 @@ fn main() {
     }
 
     let mut ans = mint::new(0);
-    let mut a1 = mint::new(0);
-    let mut a2 = mint::new(0);
-    let mut a3 = mint::new(0);
 
-    for &state in &digit_can {
+    for state in 1..1 << 10 {
         // 3 の倍数
         if count_one(state) != 3 && (state & (1 << 3) == 0) {
-            ans += dp[state][0][0];
-            ans += dp[state][0][1];
-            a1 += dp[state][0][0];
-            a1 += dp[state][0][1];
+            ans += dp[state][0][0][1];
+            ans += dp[state][0][1][1];
         }
 
         // 3 が含まれる
         if count_one(state) != 3 && (state & (1 << 3) != 0) {
             for res in 1..=2 {
-                ans += dp[state][res][0];
-                ans += dp[state][res][1];
-                a2 += dp[state][res][0];
-                a2 += dp[state][res][1];
+                ans += dp[state][res][0][1];
+                ans += dp[state][res][1][1];
             }
         }
 
         // 3 種類の数字
         if count_one(state) == 3 && (state & (1 << 3) == 0) {
             for res in 1..=2 {
-                ans += dp[state][res][0];
-                ans += dp[state][res][1];
-                a3 += dp[state][res][0];
-                a3 += dp[state][res][1];
+                ans += dp[state][res][0][1];
+                ans += dp[state][res][1][1];
             }
         }
     }
-    eprintln!("{a1} {a2} {a3}");
-    println!("{}", ans - 1); // 0 を除く
+
+    println!("{}", ans);
 }
